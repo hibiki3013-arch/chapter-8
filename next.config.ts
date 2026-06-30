5e42680921727ab0b98eaaa790efdac95ea2aaef
ひびき\宮田　響 <hibiki3013@gmail.com>
2026-06-30 18:26:04 +0900

    ９章のコードを追加

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
+// next.config.ts
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.jp',
-        port: '',
-        pathname: '/**',
-      },
+{ protocol: 'https', hostname: 'placehold.jp' },
+      { protocol: 'https', hostname: 'images.microcms-assets.io' },
     ],
     dangerouslyAllowSVG: true,
   },
 };
 
+
+
+
+ 
 export default nextConfig;
